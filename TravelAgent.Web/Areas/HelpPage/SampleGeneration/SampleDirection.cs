2c56cc14c06fe86f275a38f9c3f85a4baea285e9
Lin <h091237557@gmail.com>
2014-10-07 12:52:01 +0800

    最初版本

---
+++ TravelAgent.Web/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace TravelAgent.Web.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ TravelAgent.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TravelAgent.Web.MvcApplication" Language="C#" %>
